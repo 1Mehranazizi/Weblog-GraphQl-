d1a41cfc275ebcc1e317568ecee1d34229c08773
Mehran <info.mehranazizi@yahoo.com>
2022-07-31 22:26:43 -0700

    Set Header

---
+++ src/layouts/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Container>
+        <Toolbar>
+          <Typography component="h3" variant="h5" flex={1} fontWeight="600">
+            وب دینو
+          </Typography>
+          <IconButton color="inherit">
+            <MenuBookIcon />
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ src/theme/materialTheme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: "yekanbakh",
+    fontWeightLight: 200,
+    fontWeightRegular: 300,
+    fontWeightMedium: 400,
+    fontWeightBold: 500,
+    fontWeightHeavy: 600,
+    fontWeightFat: 700,
+  },
+  direction: "rtl",
+});
